0292c88d0281da1c44e10cdbbcdece00864fd263
Rolando Romero <rolandostar.pr@gmail.com>
2015-02-21 18:13:27 -0600

    Updated UI v0.1
    Agregadas opciones basicas.
    Colores y metodos actualizados
    Cambio a conio2 local

--- iMesias.c
+++ iMesias.c
@@ -1,34 +1,7 @@
-#include "lista.h"
-#include <conio.h>
-#include <conio2.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <string.h>
+/* ===================== LEGACY CODE ======================
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+MAIN:
 
-// Funciones Prototipo
-int printc(const char *s);
-void ShowConsoleCursor(int showFlag);
-
-lista myList;
-
-int main(int argc, char *argv[]) {
-system("COLOR 14");
-
-/*SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED |
-                                                            FOREGROUND_GREEN | 
-                                                            FOREGROUND_INTENSITY | 
-                                                            BACKGROUND_BLUE);*/
-
-  printf("  _  __  __             _              \n (_)|  \\/  |  ___  ___ (_)  __ _  ___  \n | || |\\/| | / _ \\/ __|| | / _` |/ __| \n | || |  | ||  __/\\__ \\| || (_| |\\__ \\ \n |_||_|  |_| \\___||___/|_| \\__,_||___/ \n");
-  gotoxy(4,7); printf("Opciones");
-  gotoxy(3,8); SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED); printf("Lorep ipsum dolo sit amet");
-  
-  
-   /*
-   
    int n,i;
    CANCION song;
 
@@ -63,17 +36,101 @@ system("COLOR 14");
       printf("Duracion : %s \n",song.duracion);
    }
    
-   */
-   system("PAUSE");
-   return 0;
+*/// ======================================================
+
+#include "lista.h"
+#include <conio.h>
+#include "conio2.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+
+
+// Funciones Prototipo
+int printc(const char *s);
+
+void drawUI();
+int menu();
+lista myList;
+
+int main(int argc, char *argv[]) {
+
+  SetConsoleTitle("Practica 5 - iMesias");
+  
+
+  drawUI(0); // Inicializar UI
+  gotoxy(80,22);
+  _setcursortype(_NOCURSOR);
+  /*do {
+    system("cls");
+    op = menu(vmenu,0);
+    switch(op) { 
+      case 0:
+        for(i=3;i>0;i--){
+          if(jugar(i)<=0) break; // 3 es el mas facil
+        }
+        break;
+      case 1: return 0;
+      case 27: return 0;//Hacer [ESC]
+      default: system("PAUSE"); break;
+    }
+  } while ((op!=3)&&(op!=0));*/
+  getch();
+  //system("PAUSE");
+  return 0;
+} // Fin de Main
+
+int menu(){
+  
 }
 
-void ShowConsoleCursor(int showFlag) { // set the cursor visibility
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO     cursorInfo;
-  GetConsoleCursorInfo(out, &cursorInfo);
-  cursorInfo.bVisible = showFlag; 
-    SetConsoleCursorInfo(out, &cursorInfo);
+void drawUI(int mode){
+  int i;
+  
+  if (mode == 0){
+    int x,y;
+    system("COLOR 0F");
+    switchbackground(BLACK);
+    // Logo
+    textcolor(LIGHTCYAN);
+    printf("  _  __  __             _              \n (_)|  \\/  |  ___  ___ (_)  __ _  ___  \n | || |\\/| | / _ \\/ __|| | / _` |/ __| \n | || |  | ||  __/\\__ \\| || (_| |\\__ \\ \n |_||_|  |_| \\___||___/|_| \\__,_||___/ \n");
+    // Backgrounds
+    textbackground(LIGHTBLUE);
+    for (y=0;y<=14;y++)
+      for(x=0;x<=15;x++){
+        gotoxy(x+2,y+8); printf(" ");
+      }
+    for (y=0;y<=14;y++)
+      for(x=0;x<=60;x++){
+        gotoxy(x+19,y+8); printf(" ");
+      }
+  }
+  
+  
+  //Text Options
+  textcolor(WHITE);
+  gotoxy(3,9); printf("[Biblioteca]");
+  gotoxy(16,9); printf("%c",16);
+  //Play Icon
+  gotoxy(4,11); printf("Tus Listas");
+  gotoxy(2,13); for(i=0;i<=15;i++) printf("=");
+  gotoxy(4,15); printf("Agregar");
+  gotoxy(4,17); printf("Eliminar");
+  gotoxy(4,19); printf("Editar");
+  gotoxy(4,21); printf("Salir");
+  
+  // Text Info
+  textcolor(LIGHTGRAY); textbackground(BLACK);
+  gotoxy(3,7); printf("Opciones");
+  gotoxy(19,7); printf("Lista");
+  
+  //Footer Text
+  textcolor(YELLOW);
+  gotoxy(1,24);
+  char *string;
+  asprintf(&string,"%c%c: Navegaci%cn       ESC: Salir",30,31,162);
+  printc(string);
 }
 
 int printc(const char *s) {
