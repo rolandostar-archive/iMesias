ce27d3ed88ab658ea3aa37a58d303477c732a9e0
RicardoLara <richard-o95@hotmail.com>
2015-03-02 22:12:01 -0800

    Update iMesias-Debug.c
    Solo falta Sortear :3

--- iMesias-Debug.c
+++ iMesias-Debug.c
@@ -1,4 +1,4 @@
-#include "lista.c"
+#include "lista.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +6,7 @@ lista myList;
 
 int main(){
    int n,i,pos;
-   CANCION song;
+   CANCION song,newSong;
 
    //Meter/Sacar contrarios, cola :D
    //Meter/Sacar iguales, pila :D
@@ -16,8 +16,8 @@ int main(){
    printf("Numero de elementos a pushear: "); scanf("%d",&n);
 
    for(i=0; i<n; i++){
-      printf("Nombre Artista [Sin Espacio]: "); scanf("%s",song.artista);
       printf("Nombre Cancion [Sin Espacio]: "); scanf("%s",song.nombre);
+      printf("Nombre Artista [Sin Espacio]: "); scanf("%s",song.artista);
       printf("Nombre Album   [Sin Espacio]: "); scanf("%s",song.album);
       printf("Duracion       [Sin Espacio]: "); scanf("%s",song.duracion);
       printf("Estoy metiendo en N, primero damelo: "); scanf("%d",&pos);
@@ -30,8 +30,34 @@ int main(){
    }
    printf("\n\n La lista quedo con %d elementos\n\n",size(myList));
 
-   while( !empty(myList) ){
-      song = pop_der(myList);
+   /*
+   printf("Edita uno, dame pos: "); scanf("%d",&pos);
+   printf("Data para la modificacion: ");
+   printf("Nombre Artista [Sin Espacio]: "); scanf("%s",newSong.artista);
+   printf("Nombre Cancion [Sin Espacio]: "); scanf("%s",newSong.nombre);
+   printf("Nombre Album   [Sin Espacio]: "); scanf("%s",newSong.album);
+   printf("Duracion       [Sin Espacio]: "); scanf("%s",newSong.duracion);
+   edit(myList,pos,newSong);
+
+   data(myList);
+   */
+
+   char text[70];
+   //printf("Elemento de NOMBRE CANCION a buscar: ");
+   //printf("Elemento de NOMBRE ARTISTA a buscar: ");
+   printf("Elemento de NOMBRE ALBUM a buscar: ");
+   scanf("%s",text);
+
+   // 0 para canciones  1 para Artistas y  2 para Album
+      //lista test = Search(myList,0,text);
+   lista test = search(myList,2,text);
+
+   if(!empty(test))
+   data(test);
+
+   /*while( !empty(myList) ){
+      song = pop_izq(myList);
+      //song = pop_der(myList);
       printf("-------------------------------------\n");
       printf("Saque de IZQ, size: %d \n",size(myList));
       //printf("Saque de DER, size: %d \n",size(myList));
@@ -39,5 +65,6 @@ int main(){
       printf("Artista : %s \n",song.artista);
       printf("Album : %s \n",song.album);
       printf("Duracion : %s \n",song.duracion);
-   }
+   }*/
+   return 0;
 }
