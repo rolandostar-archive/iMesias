59ad1aa774abe6dfd9a194a1cdf09b24110d6d62
RicardoLara <richard-o95@hotmail.com>
2015-03-02 22:13:51 -0800

    Update lista.c
    Falta función Sortear(myList, element)

--- lista.c
+++ lista.c
@@ -3,64 +3,138 @@
 #include<stdlib.h>
 #include<string.h>
 
-/*
-   typedef struct nodo CANCION;
-   typedef struct cab CABECERA;
-   typedef CABECERA * lista;
-
-   mensaje create(lista *);
-   boolean empty(lista);
-   void destroy(lista *);
-   void clean(lista);
-   CANCION pop_izq(lista);
-   CANCION pop_der(lista);
-   mensaje push_izq(lista, CANCION);
-   mensaje push_der(lista, CANCION);
-   mensaje push_pos(lista, int, CANCION);
-*/
-/*
 CANCION * find (lista myList, int place){
+	//inicialmente mi apuntador ve al inicio
 	CANCION * aux = myList->I;
 	int m,k=0;
-	
+   //saco la mitad de la lista
 	m = myList->size;
-	
-	if(m>pos)
+
+   //si estpy del lado izquierdo [Antes de la mitad]
+	if(m>place)
+      //Voy recorriendo aux
 		while(k++ < place)
 			aux = aux->sig;
+   //De lo contrario, aux ve al final, y como se que estoy del lado derecho de la mitar [inclusivo]
 	else{
 		aux = myList->F;
 		k = myList->size;
+		//Recorro aux para atras, y ya
 		while(k-- > place)
 			aux = aux->ant;
 	}
-	
+   //Regreso el apuntador
 	return aux;
-}*/
-/*
+}
+
+lista search(lista myList, int element, char * cad){
+
+   //Primero que no estÃ© vacÃ­a
+   printf("*********************Si entre :D Funcion Search \n\n");
+
+   CANCION * ptr = myList -> I;
+   CANCION temp;
+
+   lista result;
+   create(&result);
+
+   while( ptr != myList->F){
+      switch(element){
+         case 0:
+            if( !strcmp(ptr->nombre,cad) ){
+               temp = *ptr;
+               temp.sig = temp.ant = NULL;
+               push_der(result,temp);
+            }
+         break;
+
+         case 1:
+            if( !strcmp(ptr->artista,cad) ){
+               temp = *ptr;
+               temp.sig = temp.ant = NULL;
+               push_der(result,temp);
+            }
+         break;
+
+         case 2:
+            if( !strcmp(ptr->album,cad) ){
+               temp = *ptr;
+               temp.sig = temp.ant = NULL;
+               push_der(result,temp);
+            }
+         break;
+      }
+      ptr = ptr->sig;
+   }
+   if( !strcmp(myList->F->nombre,cad) && !element){
+      temp = *(myList->F);
+      temp.sig = temp.ant = NULL;
+      push_der(result,temp);
+   }
+   else if( !strcmp(myList->F->artista,cad) && element == 1){
+      temp = *(myList->F);
+      temp.sig = temp.ant = NULL;
+      push_der(result,temp);
+   }
+   else if( !strcmp(myList->F->album,cad) && element == 2){
+      temp = *(myList->F);
+      temp.sig = temp.ant = NULL;
+      push_der(result,temp);
+   }
+
+   printf("--------------Hazta aqui todo bien\n");
+   printf("De hecho, result queda con %d elementos\n",size(result));
+
+   return result;
+}
+
+void data(lista l){
+	if(l->I == NULL)
+		return;
+	CANCION * aux = l->I;
+	while( aux != l->F){
+		printf("********************************\n");
+		printf("Cancion : %s\n",aux->nombre);
+		printf("Artista : %s\n",aux->artista);
+		printf("Album   : %s\n",aux->album);
+		printf("Duracion: %s\n",aux->duracion);
+		aux = aux->sig;
+	}
+	//Falta el ultimo
+	printf("********************************\n");
+   printf("Cancion : %s\n",aux->nombre);
+   printf("Artista : %s\n",aux->artista);
+	printf("Album   : %s\n",aux->album);
+	printf("Duracion: %s\n",aux->duracion);
+	printf("********************************\n");
+	return;
+}
+
 void edit(lista myList, int place, CANCION song){
+	//Si es el primero
 	if(place == 0){
-		strcpy(myList->I,song.artista);
-		strcpy(myList->I,song.nombre);
-		strcpy(myList->I,song.album);
-		strcpy(myList->I,song.duracion);
+		strcpy(myList->I->artista,song.artista);
+		strcpy(myList->I->nombre,song.nombre);
+		strcpy(myList->I->album,song.album);
+		strcpy(myList->I->duracion,song.duracion);
 	}
+	//Si es el ultimo
 	else if(place >= myList->size - 1){
-		strcpy(myList->F,song.artista);
-		strcpy(myList->F,song.nombre);
-		strcpy(myList->F,song.album);
-		strcpy(myList->F,song.duracion);
+		strcpy(myList->F->artista,song.artista);
+		strcpy(myList->F->nombre,song.nombre);
+		strcpy(myList->F->album,song.album);
+		strcpy(myList->F->duracion,song.duracion);
 	}
+	//Si esta en medio
 	else{
-		CANCION * mod = NULL;
-		mod = find(myList, place);
+		CANCION * mod = find(myList, place);
 		strcpy(mod->artista,song.artista);
 		strcpy(mod->nombre,song.nombre);
 		strcpy(mod->album,song.album);
 		strcpy(mod->duracion,song.duracion);
 	}
 }
-*/
+
 int size(lista l){
    return l->size;
 }
@@ -99,58 +173,13 @@ myBoolean empty( lista q ){
    return FALSE;
 }
 
-void data(lista l,int offset){
-  //contador (final)
-int i,j;
-  //ofseet es el inicio, (empieza en cero)
-  
-	//Si vacia, hago nada
-	if(l->I == NULL)
-		return;
-	//Si no, recorro desde el inicio ( aux ) hasta el final
-	CANCION * aux = l->I;
-	for(j=0;j<offset-1;j++){
-	 aux = aux->sig;
-	}
-	int x=19,y=8;
-	//Esto no entra cuando aux = fin
-  for (i=0;i<15;i++){
-    gotoxy(x,y);
-    
-		/*printf("********************************\n");
-		printf("Artista : %s\n",aux->artista);
-		printf("Cancion : %s\n",aux->nombre);
-		printf("Album   : %s\n",aux->album);
-		printf("Duracion: %s\n",aux->duracion);*/
-	  printf("%.*s", 20, aux->nombre);
-	  gotoxy(x+20,y); printf("|");
-	  printf("%.*s", 20, aux->artista);
-	  gotoxy(x+40,y); printf("|");
-	  printf("%.*s", 15, aux->album);
-	  gotoxy(x+55,y); printf("|");
-	  printf("%.*s", 5, aux->duracion);
-	  gotoxy(x,++y);
-		aux = aux->sig;
-	}
-	
-	//Falta el ultimo, plt, lo imprimo
-	/*
-	printf("********************************\n");
-	printf("Artista : %s\n",aux->artista);
-	printf("Cancion : %s\n",aux->nombre);
-	printf("Album   : %s\n",aux->album);
-	printf("Duracion: %s\n",aux->duracion);
-	printf("********************************\n");*/
-	return;
-}
-
 CANCION pop_der(lista l){
    CANCION temp;
    //cancion que voy a regresar
    temp = *(l->F);
    free(l->F);
 
-   //si solo era un elemento, ptrs a NULL y resto al tamaño
+   //si solo era un elemento, ptrs a NULL y resto al tamaï¿½o
    if( l->I == l->F){
       l->I = l->F = NULL;
       l->size = l->size - 1;
@@ -172,7 +201,7 @@ CANCION pop_izq(lista l){
    temp = *(l->I);
    free(l->I);
 
-   //si solo era un elemento, ptrs a NULL y resto al tamaño
+   //si solo era un elemento, ptrs a NULL y resto al tamaï¿½o
    if( l->I == l->F){
       l->I = l->F = NULL;
       l->size = l->size - 1;
@@ -186,6 +215,36 @@ CANCION pop_izq(lista l){
    }
    temp.sig = temp.ant = NULL;
    return temp;
+}
+
+CANCION pop_pos(lista l, int pos){
+	//Si la posicion es mayor al num de elementos de la lista, quita el ultimo
+	//O si me dice que la quite el ultimo, pos lo quito xD
+	if(pos >= size(l))
+		return pop_der(l);
+	else if(pos==1)
+		return pop_izq(l);
+	else{
+		//Pos -- poque lo manejamos de 0 a N - 1
+		pos--;
+		int k = 0;
+		//Inicialmente apunto al primer elemento
+		CANCION * aux = l->I;
+		// Si no he llegado a destino, me muevo
+		while(k<pos){
+			aux = aux->sig;
+			k++;
+		}
+
+		CANCION temp;
+		temp = *aux;
+		(temp.ant)->sig = temp.sig;
+		(temp.sig)->ant = temp.ant;
+		temp.sig = temp.ant = NULL;
+
+		l->size = l->size - 1;
+		return temp;
+	}
 }
 
 
@@ -248,15 +307,6 @@ mensaje push_izq(lista l, CANCION song){
    l->size = l->size + 1;
 }
 
-/*
-    A B C D
-         X
-    0 1 2 3
-    1 2 3 4
-    3
-    pos = 2
-
-*/
 
 mensaje push_pos(lista l, int pos, CANCION song){
    //Si mi lista esta vacia, lo pongo al final
@@ -271,7 +321,7 @@ mensaje push_pos(lista l, int pos, CANCION song){
 
    //Si quiero meter en N y si se puede
    else{
-      
+
       //Pos -- poque lo manejamos de 0 a N - 1
       int k = 1;
       //Inicialmente apunto al primer elemento
@@ -295,7 +345,7 @@ mensaje push_pos(lista l, int pos, CANCION song){
 
       //el sig del temporal, es el sig de el aux
       temp->sig = aux->sig;
-      // El nodo al que apunta el temp, en siguiente. Éste en anterior apunta a temp 
+      // El nodo al que apunta el temp, en siguiente. ï¿½ste en anterior apunta a temp
       (temp->sig)->ant = temp;
       //El ant de temp es aux
       temp->ant = aux;
